b296014e68cadc29a6bdc053e9fd4b649cd07c8c
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-13 12:29:49 +0600

    Creating the extra reducers

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -23,6 +23,30 @@ const userSlice = createSlice({
   name: "userSlice",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.email = "";
+        state.name = "";
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.email = payload.email;
+        state.name = payload.name;
+        state.error = "";
+      })
+      .addCase(createUser.rejected, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.email = "";
+        state.name = "";
+        state.error = "";
+      });
+  },
 });
 
 export default userSlice.reducer;
